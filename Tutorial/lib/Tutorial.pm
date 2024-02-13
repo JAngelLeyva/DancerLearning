752837fff28400998db6897fa7e73d16929decdf
J Angel Leyva <joseangel.leyva@outlook.com>
2024-02-13 09:59:15 -0600

    Documentación adicional

--- Tutorial/lib/Tutorial.pm
+++ Tutorial/lib/Tutorial.pm
@@ -7,12 +7,19 @@ get '/' => sub {
     template 'index';
 };
 
+# Esta ruta es la que mostrará el formulario
 get '/formulario' => sub {
-    template '/formulario';
+    template '/formulario';    # Usa la plantlla views/formulario.tt
 };
 
+# Esta ruta procesará el formulario enviado (method="post')
 post '/formulario' => sub {
-    template '/respuesta_formulario', { nombre => params->{nombre}, apellido => params->{apellido} };
+    # Em método params permite acceder a los parámetros recibidos, internamente es un hash
+
+    # Usa la plantilla views/respuesta_formulario.tt e incluye algunas variables
+    template '/respuesta_formulario',
+      { nombre => params->{nombre}, apellido => params->{apellido}
+      }; 
 };
 
 true;
